9cf0678a3801f1fb571ba7a0154b102bb76c38e1
Ayeon-Jeong <57283339+Ayeon-Jeong@users.noreply.github.com>
2022-09-20 23:57:20 +0900

    thymeleaf 연산

--- src/main/java/mvc2/thymeleaf/basic/BasicController.java
+++ src/main/java/mvc2/thymeleaf/basic/BasicController.java
@@ -89,6 +89,16 @@ public class BasicController {
         return "basic/literal";
     }
 
+    /* 연산 */
+    @GetMapping("/operation")
+    public String operation(Model model){
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring");
+
+        return "basic/operation";
+    }
+
+
     @Data
     static class User{
         private String username;
