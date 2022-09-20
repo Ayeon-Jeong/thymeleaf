2ea0530bb48e0e93b4891af994c1fcdf7d5bb1ef
Ayeon-Jeong <57283339+Ayeon-Jeong@users.noreply.github.com>
2022-09-20 23:34:04 +0900

    thymeleaf URL 링크

--- src/main/java/mvc2/thymeleaf/basic/BasicController.java
+++ src/main/java/mvc2/thymeleaf/basic/BasicController.java
@@ -73,6 +73,15 @@ public class BasicController {
         }
     }
 
+    /* URL 링크 */
+    @GetMapping("link")
+    public String link(Model model){
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+
+        return "basic/link";
+    }
+
     @Data
     static class User{
         private String username;
