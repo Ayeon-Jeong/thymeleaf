ace2e6a8ce061c160736b230b4055df818cb1db4
jdh96 <jdh961224@gmail.com>
2022-09-20 19:47:30 +0900

    thymeleaf 날짜 유틸리티

--- src/main/java/mvc2/thymeleaf/basic/BasicController.java
+++ src/main/java/mvc2/thymeleaf/basic/BasicController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -51,10 +52,18 @@ public class BasicController {
         return "basic/variable";
 
     }
+    /* 기본 객체들 */
     @GetMapping("/basic-objects")
     public String basicObjects(HttpSession session){
         session.setAttribute("sessionData", "Hello Session");
         return "basic/basic-objects";
+    }
+
+    /* 날짜 유틸리티 */
+    @GetMapping("/date")
+    public String date(Model model){
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
     }
 
     @Component("helloBean")
